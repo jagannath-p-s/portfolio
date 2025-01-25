dd227fdbc700d3c691c0f8d177aef9989efae054
jagannath-p-s <jagannathps2000@gmail.com>
2025-01-25 18:15:06 +0530

    buildfix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Approach from "@/components/Approach";
-import Client from "@/components/Client";
+
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
@@ -11,17 +11,33 @@ import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <main className="bg-black-100 relative flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
+    <main className="bg-black relative flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav 
-        navItems={navItems} />
+        {/* Navigation */}
+        <FloatingNav navItems={navItems} />
+
+        {/* Hero Section */}
         <Hero />
+
+        {/* Grid Section */}
         <Grid />
+
+        {/* Skills Section */}
         <Skills />
+
+        {/* Recent Projects Section */}
         <RecentProjects />
-        {/* <Client /> */}
+
+        {/* Uncomment Client if required */}
+  
+
+        {/* Experience Section */}
         <Experience />
+
+        {/* Approach Section */}
         <Approach />
+
+        {/* Footer Section */}
         <Footer />
       </div>
     </main>
