a33a8957678a237d940bbfed91fea17a9b92bc16
jagannath-p-s <jagannathps2000@gmail.com>
2025-01-25 17:56:28 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,223 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Resume", link: "https://drive.google.com/file/d/1udileHqcrbD09gjuFpUZlHgpOajw3UU_/view?usp=sharing" },
+    { name: "Projects", link: "#projects" },
+    //{ name: "Testimonials", link: "#testimonials" },
+    { name: "Blogs", link: "https://dev.to/akashjana"},
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize open communication with my clients for the best quality results",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: " Flexibility is a promise from wherever you are you can count on me im just a chill guy ",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2 min-h-[40vh] select-none",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My Resume",
+      description: "Are you hiring? Check out ",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2 flex",
+      imgClassName: "",
+      titleClassName: "justify-end",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Clean Code and Modern Design",
+      description: "Passionate about transforming ideas into reality.",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+        {
+      id: 1,
+      title: "Whitesoft",
+      des: "Whitesoft is an ERP-CRM app build for Haritha Agritech Customized for their requirements",
+      img: "/haritha.png",
+      iconLists: ["/science.png",  "/tail.svg", "/supabase.png"],
+      // link: "",
+    },
+    {
+      id: 2,
+      title: "Gym CRM",
+      des: "We made a customized management app for Her Chamber Fitness , Permanagalam , with attendance tracking and reports , membership management and automated notifications , Expense and income management ,  ",
+      img: "chamber.png",
+      iconLists: ["/science.png",  "/tail.svg", "/supabase.png","/mui.svg"],
+      link: "github.com/AkashJana18/nextjs-project",
+    },
+    {
+      id: 3,
+      title: "Cryptack - Cryptocurrency tracker",
+      des: "Compare different cryptocurrencies, view their graphs—prices, total volumes, market cap, etc.",
+      img: "/regal.png",
+      iconLists: ["/html.svg","/css.svg", "/js.svg"],
+      link: "cryptack.netlify.app/",
+    },
+  
+    {
+      id: 4,
+      title: "Whitebill",
+      des: "It is a digital marketing and event planning web application , used by digital marketing agencies to scedule task to creators and editors , manage billing and event scheduling attendance tracking of staff etc ",
+      img: "/digi.png",
+      iconLists: ["/science.png",  "/tail.svg", "/supabase.png","/mui.svg"],
+      link: "react-quiz-with-timer.netlify.app/",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Vidya CollegCryptography Challenge",
+      desc: "Participated in Crytography challenge 2023 conducted at vidya college of Engineering",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Open Source",
+      desc: "Developed a React library called react-infinite-scroller which is used to create horizontal logo animations , its still in improvement stage ",
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance Project",
+      desc: "Developed a AR based web application for a client to present his machine gltf files to get funding for his big project , further info cannot be shared as i have signed a NDA",
+      className: "md:col-span-2",
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Team Led",
+      desc: "Led the team of developers at whitebranding for multiple projects ",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      href: "https://github.com/jagannath-p-s",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      href: "https://x.com/jagannathps2000",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      href: "https://www.linkedin.com/in/jagannath-ps",
+    },
+  ];
